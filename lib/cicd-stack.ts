b2db4b7255c7ff76ce4274e90e27737964e120f5
oappicgi <ossi.ala-peijari@cgi.com>
2021-12-03 22:30:44 +0200

    initial commit

---
+++ lib/cicd-stack.ts
@@ -0,0 +1,116 @@
+import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
+
+import { Construct, Stage, Stack, StageProps, SecretValue } from '@aws-cdk/core';
+import { CodePipeline, CodePipelineSource, ShellStep,ManualApprovalStep } from "@aws-cdk/pipelines";
+import * as ca from '@aws-cdk/aws-codepipeline-actions'
+import * as codepipeline from '@aws-cdk/aws-codepipeline';
+import s3deploy = require('@aws-cdk/aws-s3-deployment');
+import * as ssm from '@aws-cdk/aws-ssm';
+import * as codecommit from '@aws-cdk/aws-codecommit';
+import { GitHubSourceAction, ManualApprovalAction } from '@aws-cdk/aws-codepipeline-actions';
+import s3 = require('@aws-cdk/aws-s3');
+import * as secretsmanager from '@aws-cdk/aws-secretsmanager';
+import { ServerlessServiceStack } from './serverless-service-stack';
+import { countReset, timeStamp } from 'console';
+
+const sourceOutput = new codepipeline.Artifact();
+
+interface stageprops extends StageProps {
+  appname:string
+}
+class ApplicationStageDev extends Stage {
+  constructor(scope: Construct, id: string, props: stageprops) {
+    super(scope, id, props);
+    new ServerlessServiceStack(this, props.appname+'-service-'+"dev",{ env:props.env
+    });
+  }
+}
+
+class ApplicationStageProd extends Stage {
+  constructor(scope: Construct, id: string, props: stageprops) {
+    super(scope, id, props);
+    new ServerlessServiceStack(this, props.appname+'-service-'+"prod",{ env:props.env
+    });
+  }
+}
+
+interface ServiceStackProps extends cdk.StackProps {
+
+}
+export class CICDStack extends Stack {
+  constructor(scope: Construct, id: string, props: ServiceStackProps) {
+    super(scope, id, props);
+    const appname = this.node.tryGetContext('appname')  
+    const sourceArtifact = new codepipeline.Artifact();
+    const cloudAssemblyArtifact = new codepipeline.Artifact();
+    
+    new ssm.StringParameter(this, 'ServiceNow-GitConnectionParameter', {
+      description: 'ARN of the github connection that connects pipelineToRepository after manually setting it up',
+      parameterName: '/servicenow/gitconnectionARN',
+      stringValue: 'InsertMeAfterInitPhase',
+      tier: ssm.ParameterTier.STANDARD,
+    });
+
+/*
+Old method of getting access to github, should be replaced with ServiceNow-GitConnectionParameter... read todo from code for reason
+*/
+
+    const secretToken = new secretsmanager.Secret(this, appname + '-AppSecrets',
+    {
+      generateSecretString: {
+        secretStringTemplate: '{"gittoken": "token"}', 
+        generateStringKey: 'password',
+      },
+    },
+  );
+
+  if (this.node.tryGetContext('phase') != "init") {
+
+    var branch = "master"
+
+    //TODO input in pipeline should be replaced with this. Current method is based on personal token which means that when person looses access to Väylä Git, Pipeline stops working
+    // New method requires connection be crated with organization admin priviliges
+    /* 
+          input: CodePipelineSource.connection(this.node.tryGetContext('ownerandapp'), 'master', {
+            connectionArn: ssm.StringParameter.fromStringParameterAttributes(this, appname+'-RetrivedGitConnectionParameter', {
+              parameterName: '/'+appname+'/gitARN',
+            }).stringValue, // Created using the AWS console in init phase * });',
+
+    */
+   
+
+        const pipeline = new CodePipeline(this, appname+'-Pipeline', {
+          crossAccountKeys:true,
+          pipelineName: appname+'-Pipeline',
+          synth:  new ShellStep('Synth', {
+          //  input: CodePipelineSource.codeCommit(repo, branch),
+          input: CodePipelineSource.gitHub(this.node.tryGetContext('ownerandapp'), 'master', {
+            authentication: SecretValue.secretsManager(secretToken.secretArn,{jsonField:"gittoken"})
+            }),
+            
+          commands: ['npm ci', 'npm run build', 'npx cdk synth'],            
+          })
+        });
+
+    /*
+repo - to read the repository
+admin:repo_hook - if you plan to use webhooks (true by default)
+*/
+    
+
+
+   const deployStage = pipeline.addStage( new ApplicationStageDev(this, appname+"dev-deploy", {
+      env: { account: this.account, region: this.region }, 
+      appname:appname
+    },
+    )); 
+    const proddeployStage = pipeline.addStage(new ApplicationStageProd(this, appname+"prod-deploy", {
+      env: { account: this.account, region: this.region },
+      appname:appname    
+    }));
+    proddeployStage.addPre(new ManualApprovalStep('Pre-production check'))    
+  }  
+  }
+  }
+
---
+++ lib/serverless-service-stack.ts
@@ -0,0 +1,95 @@
+import * as cdk from '@aws-cdk/core';
+import * as iam from '@aws-cdk/aws-iam';
+import { CfnOutput, Construct, StackProps, Stage, Tag } from '@aws-cdk/core';
+import s3 = require('@aws-cdk/aws-s3');
+import * as lambda from '@aws-cdk/aws-lambda';
+import { LambdaFunction } from '@aws-cdk/aws-events-targets';
+import * as s3n from '@aws-cdk/aws-s3-notifications';
+import { Rule, Schedule } from '@aws-cdk/aws-events';
+import * as secretsmanager from '@aws-cdk/aws-secretsmanager';
+
+export class ServerlessServiceStack extends cdk.Stack {
+    constructor(scope: cdk.Construct, id: string, props?: StackProps) {
+      super(scope, id, props);
+      const lambdaRole = iam.Role.fromRoleArn(
+        this,
+        'imported-role',
+        `arn:aws:iam::${cdk.Stack.of(this).account}:role/servicenowlambda`,
+        {mutable: true},
+      );   
+
+      const secretmanagerForSecrets = new secretsmanager.Secret(this,"APISecrets" + this.stackName,
+      { //DO NOT change this object, it will clear secrets, manually add secrets to it and query them from lambda. 
+        generateSecretString: {
+          secretStringTemplate: '{"templatekey": "templatevalue"}', 
+          generateStringKey: 'password',
+        },
+      },
+    );
+      
+      datapipeServiceNowTable(this,"now/table/task",this.stackName,secretmanagerForSecrets,this.region,
+      lambdaRole,"Servicenow-Table-Task-URL","Servicenow-Table-Task-Username","Servicenow-Table-Task-password") 
+      //remember to add url,username,password hints to secretmanager so lambda can fetch them
+}
+}
+
+function datapipeServiceNowTable(construct:cdk.Construct,rajapintaName:string, appnameAndEnv:string ,secretmanager: secretsmanager.Secret,region:string,lambdaRole:iam.IRole,urlsecretHint:string,secretusernameHint:string,secretpasswordHint:string){
+  const resourcenaming= "-"+rajapintaName+"-"+appnameAndEnv
+     
+  const landingBucket = new s3.Bucket(construct, 'LandingBucket'+resourcenaming,{
+    removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+    const databucket = new s3.Bucket(construct, 'DataBucket'+resourcenaming,{
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+/*    
+  const apiLambda=new lambda.Function(construct, 'APIFetch'+resourcenaming, {
+    code: lambda.Code.fromAsset("./lambdas/api/path/", ),
+    handler: "com.cgi.lambda.apifetch.LambdaFunctionHandler",
+    runtime:lambda.Runtime.JAVA_8,
+    environment:{
+    "secrets": secretmanager.secretArn, 
+    "region": region,
+    "alert_string":"## VAYLA AWS-HALYTYS: SERVICENOW:",
+    "charset": "UTF-8",
+    "password": secretpasswordHint,
+    "username": secretusernameHint,
+    "service_url": urlsecretHint,
+    "splitlimit": "1500",
+    "s3_bucket_name":landingBucket.bucketName    
+  },
+    role:lambdaRole
+  }); 
+  secretmanager.grantRead(apiLambda)
+  landingBucket.grantPut(apiLambda)
+*/
+
+  /* Add these if two phased prosessing
+  const converterLambda=new lambda.Function(construct, 'ADEConverter'+resourcenaming, {
+    code: lambda.Code.fromAsset("./lambdas/converterlambda/path/", ),
+    handler: "com.cgi.lambda.apifetch.LambdaFunctionHandler",
+    environment: { 
+      "DevARN": construct.node.tryGetContext('ADEDevACL'),"PRODARN": construct.node.tryGetContext('ADEPRODACL'),"QAARN": construct.node.tryGetContext('ADEQAACL'),
+     "is_dst": "", "not_dst": "" },
+    runtime: lambda.Runtime.JAVA_8,
+  });  
+  landingBucket.addEventNotification(s3.EventType.OBJECT_CREATED, new s3n.LambdaDestination(converterLambda))
+ 
+  databucket.grantPut(converterLambda)
+  landingBucket.grantRead(converterLambda)
+  secretmanager.grantRead(apiLambda)
+*/
+
+  const rule= new Rule(construct,"dailyRun"+resourcenaming, {
+  schedule: Schedule.expression("cron(15 3 * * ? *)"),
+//  targets: [new LambdaFunction(apiLambda)],
+});
+cdk.Tags.of(landingBucket).add("APIFetch",rajapintaName)
+cdk.Tags.of(databucket).add("APIFetch",rajapintaName)
+//cdk.Tags.of(apiLambda).add("APIFetch",rajapintaName)
+cdk.Tags.of(rule).add("APIFetch",rajapintaName)
+}
+//stepfunction (optional, nice to have no time atm)
+
+
+
